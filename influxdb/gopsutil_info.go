906636c5fee5535c74cfcf320f012e2e45ab4f1b
jokereven <zjing125.cn@gmail.com>
2022-06-22 23:00:08 +0800

    feat: 完成grafana所有数据展示

--- influxdb/gopsutil_info.go
+++ influxdb/gopsutil_info.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	client "github.com/influxdata/influxdb1-client/v2"
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
 	"log"
 	"time"
+)
+
+var (
+	cli                     client.Client
+	lastNetIOStartTimeStamp int64    // 上一次获取网络io的时间
+	lastNetIoInfo           *NetInfo // 上一次网络io的数据
 )
 
 const (
@@ -16,16 +24,19 @@ const (
 	NetInfoType  = "net"
 )
 
-type CpuInfo struct {
-	CpuPercent float64 `json:"cpu_percent"`
-}
-
+// SysInfo 系统信息
 type SysInfo struct {
 	InfoType string
 	IP       string
 	Data     interface{}
 }
 
+// CpuInfo cpu信息
+type CpuInfo struct {
+	CpuPercent float64 `json:"cpu_percent"`
+}
+
+// MemInfo 内存信息
 type MemInfo struct {
 	Total       uint64  `json:"total"`
 	Available   uint64  `json:"available"`
@@ -35,9 +46,41 @@ type MemInfo struct {
 	Cached      uint64  `json:"cached"`
 }
 
-var (
-	cli client.Client
-)
+// UsageStat 分区信息
+type UsageStat struct {
+	Path              string  `json:"path"`
+	Fstype            string  `json:"fstype"`
+	Total             uint64  `json:"total"`
+	Free              uint64  `json:"free"`
+	Used              uint64  `json:"used"`
+	UsedPercent       float64 `json:"used_percent"`
+	InodesTotal       uint64  `json:"inodes_total"`
+	InodesUsed        uint64  `json:"inodes_used"`
+	InodesFree        uint64  `json:"inodes_free"`
+	InodesUsedPercent float64 `json:"inodes_used_percent"`
+}
+
+// DiskInfo 磁盘信息
+type DiskInfo struct {
+	PartitionUsageStat map[string]*disk.UsageStat
+}
+
+// IOStat 网络io
+type IOStat struct {
+	BytesSent       uint64
+	BytesRecv       uint64
+	PacketsSent     uint64
+	PacketsRecv     uint64
+	BytesSentRate   float64 `json:"bytes_sent_rate"`
+	BytesRecvRate   float64 `json:"bytes_recv_rate"`
+	PacketsSentRate float64 `json:"packets_sent_rate"`
+	PacketsRecvRate float64 `json:"packets_recv_rate"`
+}
+
+// NetInfo 网络信息
+type NetInfo struct {
+	NetIOCountersStat map[string]*IOStat
+}
 
 // 使用 gopustuil 监控cpu的情况并写入influxdb
 
@@ -108,7 +151,73 @@ func writesPointsMemToDB(info *MemInfo) {
 	log.Println("insert mem info success")
 }
 
-// 获取 cpu 的信息
+// writesPointsDiskToDB
+func writesPointsDiskToDB(info *DiskInfo) {
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  "monitor",
+		Precision: "s", //精度，默认ns
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for k, v := range info.PartitionUsageStat {
+		tags := map[string]string{"path": k}
+		fields := map[string]interface{}{
+			"total":               int64(v.Total),
+			"free":                int64(v.Free),
+			"used":                int64(v.Used),
+			"used_percent":        v.UsedPercent,
+			"inodes_total":        int64(v.InodesTotal),
+			"inodes_used":         int64(v.InodesUsed),
+			"inodes_free":         int64(v.InodesFree),
+			"inodes_used_percent": v.InodesUsedPercent,
+		}
+		pt, err := client.NewPoint("disk", tags, fields, time.Now())
+		if err != nil {
+			log.Fatal(err)
+		}
+		bp.AddPoint(pt)
+	}
+
+	err = cli.Write(bp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("insert disk info success")
+}
+
+// writesPointsNetToDB
+func writesPointsNetToDB(info *NetInfo) {
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  "monitor",
+		Precision: "s", //精度，默认ns
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for k, v := range info.NetIOCountersStat {
+		tags := map[string]string{"name": k}
+		fields := map[string]interface{}{
+			"bytes_sent_rate":   v.BytesSentRate,
+			"bytes_recv_rate":   v.BytesRecvRate,
+			"packets_sent_rate": v.PacketsSentRate,
+			"packets_recv_rate": v.PacketsRecvRate,
+		}
+		pt, err := client.NewPoint("net", tags, fields, time.Now())
+		if err != nil {
+			log.Fatal(err)
+		}
+		bp.AddPoint(pt)
+	}
+
+	err = cli.Write(bp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("insert net info success")
+}
+
+// cpu info（cpu信息）
 func getCpuInfo() {
 	var cpuInfo = new(CpuInfo)
 	// CPU使用率
@@ -119,7 +228,7 @@ func getCpuInfo() {
 	writesPointsCpuToDB(cpuInfo)
 }
 
-// mem info
+//  mem info（内存信息）
 func getMemInfo() {
 	var MemInfo = new(MemInfo)
 	info, err := mem.VirtualMemory()
@@ -135,6 +244,65 @@ func getMemInfo() {
 	writesPointsMemToDB(MemInfo)
 }
 
+// Disk info（磁盘信息）
+func getDiskInfo() {
+	var diskInfo = &DiskInfo{
+		PartitionUsageStat: make(map[string]*disk.UsageStat, 16),
+	}
+	parts, _ := disk.Partitions(true)
+	for _, part := range parts {
+		// 拿到每一个分区
+		usageData, err := disk.Usage(part.Mountpoint) // 传挂载点
+		if err != nil {
+			fmt.Printf("get %s part mount_point failed, err:%v ", part.Mountpoint, err)
+			continue
+		}
+		diskInfo.PartitionUsageStat[part.Mountpoint] = usageData
+		writesPointsDiskToDB(diskInfo)
+	}
+}
+
+// net info（网络信息）
+func getNetInfo() {
+	var netInfo = &NetInfo{
+		NetIOCountersStat: make(map[string]*IOStat, 8),
+	}
+	// 获取当前时间
+	currenttime := time.Now().Unix()
+	netIOs, err := net.IOCounters(true)
+	if err != nil {
+		fmt.Println("get net io failed, err: ", err)
+		return
+	}
+	for _, netIO := range netIOs {
+		var ioStat = new(IOStat)
+		ioStat.BytesSent = netIO.BytesSent
+		ioStat.BytesRecv = netIO.BytesRecv
+		ioStat.PacketsSent = netIO.PacketsSent
+		ioStat.PacketsRecv = netIO.PacketsRecv
+
+		// 将具体网卡数据的isStat变量添加到map中
+		netInfo.NetIOCountersStat[netIO.Name] = ioStat
+
+		// 开启计算网卡相关数据
+		// 第一次不进行计算
+		if lastNetIOStartTimeStamp == 0 || lastNetIoInfo == nil {
+			continue
+		}
+		// 时间间隔
+		interval := currenttime - lastNetIOStartTimeStamp
+		ioStat.BytesSentRate = (float64(ioStat.BytesSent) - float64(lastNetIoInfo.NetIOCountersStat[netIO.Name].BytesSent)) / float64(interval)
+		ioStat.BytesRecvRate = (float64(ioStat.BytesRecv) - float64(lastNetIoInfo.NetIOCountersStat[netIO.Name].BytesRecv)) / float64(interval)
+		ioStat.PacketsSentRate = (float64(ioStat.PacketsSent) - float64(lastNetIoInfo.NetIOCountersStat[netIO.Name].PacketsSent)) / float64(interval)
+		ioStat.PacketsSentRate = (float64(ioStat.PacketsRecv) - float64(lastNetIoInfo.NetIOCountersStat[netIO.Name].PacketsRecv)) / float64(interval)
+	}
+	// 更新全局记录的上一次采集网卡数据
+	lastNetIOStartTimeStamp = currenttime // 更新时间
+	lastNetIoInfo = netInfo
+	// 发送到influxDB
+	writesPointsNetToDB(netInfo)
+}
+
 func main() {
 	err := connInfluxDB()
 	if err != nil {
@@ -144,7 +312,9 @@ func main() {
 	ticker := time.Tick(1 * time.Second)
 	for {
 		<-ticker
-		//getCpuInfo()
+		getCpuInfo()
 		getMemInfo()
+		getDiskInfo()
+		getNetInfo()
 	}
 }
